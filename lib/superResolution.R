21146742b859fba49d689484c54591aa341ace6c
OKKING <jm4610@columbia.edu>
2018-11-04 00:08:01 -0400

    Update Superresolution
    Update Superresolution

--- lib/superResolution.R
+++ lib/superResolution.R
@@ -14,19 +14,21 @@ superResolution <- function(LR_dir, HR_dir, modelList){
   
   ### load libraries
   library("EBImage")
-  n_files <- 1# length(list.files(LR_dir))
+  n_files <- length(list.files(LR_dir))
   
   ### read LR/HR image pairs
   for(i in 1:n_files){
     imgLR <- readImage(paste0(LR_dir,  "img", "_", sprintf("%04d", i), ".jpg"))
     pathHR <- paste0(HR_dir,  "img", "_", sprintf("%04d", i), ".jpg")
-    featMat <- array(NA, c(dim(imgLR)[1] * dim(imgLR)[2], 8, 3))
+    n<-dim(imgLR)[1]
+    m<-dim(imgLR)[2]
+    featMat <- array(NA, c(n*m, 8, 3))
     
     imgLR <- as.array(imgLR)
-    select=dim(imgLR)[1] * dim(imgLR)[2]
-    n_points=length(select)
-    select_row=(select-1)%%dim(imgLR)[1]+1
-    select_col=(select-1)%/%dim(imgLR)[1]+1
+    select=c(1:(n*m))
+    #n_points=length(select)
+    select_row=(select-1)%%n +1
+    select_col=(select-1)%/%n +1
     
     ### step 1. for each pixel and each channel in imgLR:
     ###           save (the neighbor 8 pixels - central pixel) in featMat
@@ -35,20 +37,43 @@ superResolution <- function(LR_dir, HR_dir, modelList){
       pad=cbind(0,imgLR[,,j],0)
       pad=rbind(0,pad,0)
       center=pad[cbind(select_row+1,select_col+1)]
-      featMat[(i-1)*n_points+1:n_points,1,j]=pad[cbind(select_row,select_col)]-center
-      featMat[(i-1)*n_points+1:n_points,2,j]=pad[cbind(select_row,select_col+1)]-center
-      featMat[(i-1)*n_points+1:n_points,3,j]=pad[cbind(select_row,select_col+2)]-center
-      featMat[(i-1)*n_points+1:n_points,4,j]=pad[cbind(select_row+1,select_col+2)]-center
-      featMat[(i-1)*n_points+1:n_points,5,j]=pad[cbind(select_row+2,select_col+2)]-center
-      featMat[(i-1)*n_points+1:n_points,6,j]=pad[cbind(select_row+2,select_col+1)]-center
-      featMat[(i-1)*n_points+1:n_points,7,j]=pad[cbind(select_row+2,select_col)]-center
-      featMat[(i-1)*n_points+1:n_points,8,j]=pad[cbind(select_row+1,select_col)]-center
+      featMat[,1,j]=pad[cbind(select_row,select_col)] - center
+      featMat[,2,j]=pad[cbind(select_row,select_col+1)]- center
+      featMat[,3,j]=pad[cbind(select_row,select_col+2)]- center
+      featMat[,4,j]=pad[cbind(select_row+1,select_col+2)]- center
+      featMat[,5,j]=pad[cbind(select_row+2,select_col+2)]- center
+      featMat[,6,j]=pad[cbind(select_row+2,select_col+1)]- center
+      featMat[,7,j]=pad[cbind(select_row+2,select_col)]- center
+      featMat[,8,j]=pad[cbind(select_row+1,select_col)]- center
     }
     
     ### step 2. apply the modelList over featMat
     predMat <- test(modelList, featMat)
     ### step 3. recover high-resolution from predMat and save in HR_dir
-    HRimg<-Image(predMat,colormode = Color)
+    if (dim(predMat)[1]== m*n){
+      index<-c(1:(m*n))
+      index2<-1:m
+      featImg<- array(NA, c(2*n, 2*m, 3))
+      for (k in 1:3){
+        
+        vec_odd<-numeric(2*m*n)
+        vec_odd[c(2*index-1)]<-as.vector(predMat[,1,k])
+        vec_odd[2*index]<-as.vector(predMat[,2,k])
+        vec_even<-numeric(2*m*n)
+        vec_even[c(2*index-1)]<-as.vector(predMat[,3,k])
+        vec_even[2*index]<-as.vector(predMat[,4,k])
+        mat_odd<-matrix(vec_odd,nrow = 2*n,ncol = m)
+        mat_even<-matrix(vec_even,nrow = 2*n,ncol = m)
+        
+        imgMAt<-matrix(NA,nrow = 2*n,ncol = 2*m)
+        
+        imgMAt[,c(2*index2-1)]<-mat_odd
+        imgMAt[,2*index2]<-mat_even
+        
+        featImg[,,k]<-imgMAt
+    }}
+    
+    HRimg<-Image(featImg,colormode = Color)
     save(HRimg, file=pathHR)
     
     
