1c5b1775691e83fb6c0d8054d1fb374d2d0235a5
OKKING <jm4610@columbia.edu>
2018-11-04 00:21:03 -0400

    fix feture
    fix feature

--- lib/feature.R
+++ lib/feature.R
@@ -39,22 +39,22 @@ feature <- function(LR_dir, HR_dir, n_points=1000){
     for(j in 1:3){
         pad=cbind(0,imgLR[,,j],0)
         pad=rbind(0,pad,0)
-        center=pad[cbind(select_row+1,select_col+1)]
-        featMat[(i-1)*n_points+1:n_points,1,j]=pad[cbind(select_row,select_col)]-center
-        featMat[(i-1)*n_points+1:n_points,2,j]=pad[cbind(select_row,select_col+1)]-center
-        featMat[(i-1)*n_points+1:n_points,3,j]=pad[cbind(select_row,select_col+2)]-center
-        featMat[(i-1)*n_points+1:n_points,4,j]=pad[cbind(select_row+1,select_col+2)]-center
-        featMat[(i-1)*n_points+1:n_points,5,j]=pad[cbind(select_row+2,select_col+2)]-center
-        featMat[(i-1)*n_points+1:n_points,6,j]=pad[cbind(select_row+2,select_col+1)]-center
-        featMat[(i-1)*n_points+1:n_points,7,j]=pad[cbind(select_row+2,select_col)]-center
-        featMat[(i-1)*n_points+1:n_points,8,j]=pad[cbind(select_row+1,select_col)]-center
+        #center=pad[cbind(select_row+1,select_col+1)]
+        featMat[(i-1)*n_points+1:n_points,1,j]=pad[cbind(select_row,select_col)]
+        featMat[(i-1)*n_points+1:n_points,2,j]=pad[cbind(select_row,select_col+1)]
+        featMat[(i-1)*n_points+1:n_points,3,j]=pad[cbind(select_row,select_col+2)]
+        featMat[(i-1)*n_points+1:n_points,4,j]=pad[cbind(select_row+1,select_col+2)]
+        featMat[(i-1)*n_points+1:n_points,5,j]=pad[cbind(select_row+2,select_col+2)]
+        featMat[(i-1)*n_points+1:n_points,6,j]=pad[cbind(select_row+2,select_col+1)]
+        featMat[(i-1)*n_points+1:n_points,7,j]=pad[cbind(select_row+2,select_col)]
+        featMat[(i-1)*n_points+1:n_points,8,j]=pad[cbind(select_row+1,select_col)]
         ### step 2.2. save the corresponding 4 sub-pixels of imgHR in labMat
         channelHR=imgHR[,,j]
         
-        labMat[(i-1)*n_points+1:n_points,1,j]=channelHR[cbind(select_row*2-1,select_col*2-1)]-center
-        labMat[(i-1)*n_points+1:n_points,2,j]=channelHR[cbind(select_row*2-1,select_col*2)]-center
-        labMat[(i-1)*n_points+1:n_points,3,j]=channelHR[cbind(select_row*2,select_col*2)]-center
-        labMat[(i-1)*n_points+1:n_points,4,j]=channelHR[cbind(select_row*2,select_col*2-1)]-center
+        labMat[(i-1)*n_points+1:n_points,1,j]=channelHR[cbind(select_row*2-1,select_col*2-1)]
+        labMat[(i-1)*n_points+1:n_points,2,j]=channelHR[cbind(select_row*2-1,select_col*2)]
+        labMat[(i-1)*n_points+1:n_points,3,j]=channelHR[cbind(select_row*2,select_col*2)]
+        labMat[(i-1)*n_points+1:n_points,4,j]=channelHR[cbind(select_row*2,select_col*2-1)]
     ### step 3. repeat above for three channels
     }
   }
