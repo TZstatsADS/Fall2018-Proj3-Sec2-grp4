b3ae6de3227e7c7355fb81f7d87edd5f9bd07bee
YangYang <jokerkeny@gmail.com>
2018-11-06 22:21:07 -0500

    Minus center in feature, label, Add it back on pred
    the previous effect without +/- center is not very good, as it lacks center info

--- lib/feature_parallel.R
+++ lib/feature_parallel.R
@@ -43,22 +43,22 @@ feature <- function(LR_dir, HR_dir, n_points=1000){
     for(j in 1:3){
         pad=cbind(0,imgLR[,,j],0)
         pad=rbind(0,pad,0)
-        #center=pad[cbind(select_row+1,select_col+1)]
-        featMat[1:n_points,1,j]=pad[cbind(select_row,select_col)]
-        featMat[1:n_points,2,j]=pad[cbind(select_row,select_col+1)]
-        featMat[1:n_points,3,j]=pad[cbind(select_row,select_col+2)]
-        featMat[1:n_points,4,j]=pad[cbind(select_row+1,select_col+2)]
-        featMat[1:n_points,5,j]=pad[cbind(select_row+2,select_col+2)]
-        featMat[1:n_points,6,j]=pad[cbind(select_row+2,select_col+1)]
-        featMat[1:n_points,7,j]=pad[cbind(select_row+2,select_col)]
-        featMat[1:n_points,8,j]=pad[cbind(select_row+1,select_col)]
+        center=pad[cbind(select_row+1,select_col+1)]
+        featMat[,1,j]=pad[cbind(select_row,select_col)]-center
+        featMat[,2,j]=pad[cbind(select_row,select_col+1)]-center
+        featMat[,3,j]=pad[cbind(select_row,select_col+2)]-center
+        featMat[,4,j]=pad[cbind(select_row+1,select_col+2)]-center
+        featMat[,5,j]=pad[cbind(select_row+2,select_col+2)]-center
+        featMat[,6,j]=pad[cbind(select_row+2,select_col+1)]-center
+        featMat[,7,j]=pad[cbind(select_row+2,select_col)]-center
+        featMat[,8,j]=pad[cbind(select_row+1,select_col)]-center
         ### step 2.2. save the corresponding 4 sub-pixels of imgHR in labMat
         channelHR=imgHR[,,j]
         
-        labMat[1:n_points,1,j]=channelHR[cbind(select_row*2-1,select_col*2-1)]
-        labMat[1:n_points,2,j]=channelHR[cbind(select_row*2-1,select_col*2)]
-        labMat[1:n_points,3,j]=channelHR[cbind(select_row*2,select_col*2)]
-        labMat[1:n_points,4,j]=channelHR[cbind(select_row*2,select_col*2-1)]
+        labMat[,1,j]=channelHR[cbind(select_row*2-1,select_col*2-1)]-center
+        labMat[,2,j]=channelHR[cbind(select_row*2-1,select_col*2)]-center
+        labMat[,3,j]=channelHR[cbind(select_row*2,select_col*2)]-center
+        labMat[,4,j]=channelHR[cbind(select_row*2,select_col*2-1)]-center
     ### step 3. repeat above for three channels
     }
     abind(featMat,labMat, along=2)
--- lib/superResolution.R
+++ lib/superResolution.R
@@ -23,6 +23,7 @@ superResolution <- function(LR_dir, HR_dir, modelList){
     n<-dim(imgLR)[1]
     m<-dim(imgLR)[2]
     featMat <- array(NA, c(n*m, 8, 3))
+    center=featMat
     
     imgLR <- as.array(imgLR)
     select=c(1:(n*m))
@@ -37,6 +38,7 @@ superResolution <- function(LR_dir, HR_dir, modelList){
       pad=cbind(0,imgLR[,,j],0)
       pad=rbind(0,pad,0)
       
+      #center=pad[cbind(select_row+1,select_col+1)]
       featMat[,1,j]=pad[cbind(select_row,select_col)] 
       featMat[,2,j]=pad[cbind(select_row,select_col+1)]
       featMat[,3,j]=pad[cbind(select_row,select_col+2)]
@@ -45,10 +47,19 @@ superResolution <- function(LR_dir, HR_dir, modelList){
       featMat[,6,j]=pad[cbind(select_row+2,select_col+1)]
       featMat[,7,j]=pad[cbind(select_row+2,select_col)]
       featMat[,8,j]=pad[cbind(select_row+1,select_col)]
+      ### - center
+      center[,,j]=imgLR[,,j]
+      featMat[,,j]=featMat[,,j]-center[,,j]
     }
     
     ### step 2. apply the modelList over featMat
     predMat <- test(modelList, featMat)
+    center=predMat
+    ### + center
+    for(j in 1:3){
+      center[,,j]=imgLR[,,j]
+      predMat[,,j]=predMat[,,j]+center[,,j]
+    }
     ### step 3. recover high-resolution from predMat and save in HR_dir
     if (dim(predMat)[1]== m*n){
       index<-c(1:(m*n))
